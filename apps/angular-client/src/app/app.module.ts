d9fefb8972c27454a2fce05ca1704baa870757b3
Oleh Biblyi <o.biblyi@levi9.com>
2022-01-31 13:58:20 +0200

    init client shell lib

--- apps/angular-client/src/app/app.module.ts
+++ apps/angular-client/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ClientShellModule } from '@nx/client/shell';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ClientShellModule],
   providers: [],
   bootstrap: [AppComponent],
 })
