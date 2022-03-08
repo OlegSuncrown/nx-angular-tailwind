4ad5a22b4a7daa17e3992798b1f966b296410ca8
Oleh Biblyi <o.biblyi@levi9.com>
2022-03-08 18:23:41 +0200

    add custom slider

--- libs/client/ui/audio-slider/src/lib/audio-slider/audio-slider.component.ts
+++ libs/client/ui/audio-slider/src/lib/audio-slider/audio-slider.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'nx-audio-slider',
   templateUrl: './audio-slider.component.html',
-  styleUrls: ['./audio-slider.component.scss']
+  styleUrls: ['./audio-slider.component.scss'],
 })
 export class AudioSliderComponent {
+  @ViewChild('progressBar')
+  progressBar: ElementRef | undefined;
 
-  constructor() { }
+  width: any = '0%';
+  thumbWidth = 15;
+  constructor() {}
 
   // ngOnInit(): void {
   // }
 
   onChange($event: any) {
-    console.log($event)
+    // this.width = $event.target.value + '%'
+    const currentValue = $event.target.value;
+    const progressBarWidth = this.progressBar?.nativeElement.getBoundingClientRect().width
+    this.width = this.thumbWidth + progressBarWidth/100 * currentValue - (this.thumbWidth/100 * currentValue) + 'px';
+    console.log(this.width);
   }
 }
