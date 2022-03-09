46b62818f96773dd1c879d1734230135dba78fd3
Oleh Biblyi <o.biblyi@levi9.com>
2022-03-09 12:50:25 +0200

    style slider

--- libs/client/ui/audio-slider/src/lib/audio-slider/audio-slider.component.ts
+++ libs/client/ui/audio-slider/src/lib/audio-slider/audio-slider.component.ts
@@ -1,26 +1,27 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'nx-audio-slider',
   templateUrl: './audio-slider.component.html',
   styleUrls: ['./audio-slider.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AudioSliderComponent {
   @ViewChild('progressBar')
   progressBar: ElementRef | undefined;
-
-  width: any = '0%';
-  thumbWidth = 15;
+  height = 4;
+  trackFillWidth = 0;
+  thumbWidth = 10;
+  thumbHeight = 10;
+  
   constructor() {}
 
   // ngOnInit(): void {
   // }
 
   onChange($event: any) {
-    // this.width = $event.target.value + '%'
     const currentValue = $event.target.value;
     const progressBarWidth = this.progressBar?.nativeElement.getBoundingClientRect().width
-    this.width = this.thumbWidth + progressBarWidth/100 * currentValue - (this.thumbWidth/100 * currentValue) + 'px';
-    console.log(this.width);
+    this.trackFillWidth = this.thumbWidth + progressBarWidth/100 * currentValue - (this.thumbWidth/100 * currentValue);
   }
 }
