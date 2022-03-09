45230154df034e141e62ef9e4aa0e00efa43a314
Oleh Biblyi <o.biblyi@levi9.com>
2022-03-09 19:09:32 +0200

    create audio object

--- libs/client/data-access/player-store/player-store/src/lib/player-store.store.ts
+++ libs/client/data-access/player-store/player-store/src/lib/player-store.store.ts
@@ -1,17 +1,43 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
+import { fromEvent } from 'rxjs';
 
 export interface PlayerStoreState {
-  audioFile: any
-};
+  audioObj: HTMLAudioElement;
+}
 
 const initialState: PlayerStoreState = {
-  audioFile: ''
+  audioObj: new Audio(),
 };
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class PlayerStoreStore extends ComponentStore<PlayerStoreState> {
+  audioEvents = [
+    "ended",
+    "error",
+    "play",
+    "playing",
+    "pause",
+    "timeupdate",
+    "canplay",
+    "loadedmetadata",
+    "loadstart"
+  ];
+  
+  song = 'https://levi9-song-quiz.herokuapp.com/api/audio/4-1.mp3'
+  audioObj = new Audio()
+  currentTime$ = fromEvent(this.audioObj, this.audioEvents[5])
+
   constructor() {
     super(initialState);
   }
+
+  play() {
+    this.audioObj.src = this.song;
+    this.audioObj.load();
+    this.audioObj.play();
+  }
+
 }
--- libs/client/features/feature-quiz/src/lib/quiz/quiz.component.ts
+++ libs/client/features/feature-quiz/src/lib/quiz/quiz.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { PlayerStoreStore } from '@nx/client/data-access/player-store/player-store';
 import * as quizStore from '@nx/client/data-access/quiz-store';
 
 @Component({
@@ -11,15 +12,19 @@ import * as quizStore from '@nx/client/data-access/quiz-store';
 export class QuizComponent implements OnInit {
   game$ = this.store.select(quizStore.selectGameQuizState);
   currentQuestion$ = this.store.select(quizStore.selectCurrentQuestion);
-  currentGenre$ = this.store.select(quizStore.selectCurrentGenre)
+  currentGenre$ = this.store.select(quizStore.selectCurrentGenre);
   progressAnimation$ = this.store.select(quizStore.selectProgressAnimation);
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, private playerStore: PlayerStoreStore) {}
   ngOnInit(): void {
     this.store.dispatch(quizStore.loadQuizData());
+    this.playerStore.currentTime$.subscribe((val: any) => {
+      console.log('TICK', this.playerStore.audioObj.currentTime);
+    });
   }
 
   onStartNewLevel() {
+    this.playerStore.play();
     this.store.dispatch(quizStore.nextLevel());
   }
 }
