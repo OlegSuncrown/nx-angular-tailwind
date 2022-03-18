69b09b112a5b2d2fdb9e08a3bc941f36e02f8d67
Oleh Biblyi <o.biblyi@levi9.com>
2022-03-18 16:22:58 +0200

    add basic styles to summary page

--- libs/client/features/feature-summary/src/lib/client-features-feature-summary.module.ts
+++ libs/client/features/feature-summary/src/lib/client-features-feature-summary.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { SummaryComponent } from './summary/summary.component';
+import { ClientUiQuizButtonModule } from '@nx/client/ui/quiz-button';
 
 const routes: Routes = [{ path: '', component: SummaryComponent }];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, ClientUiQuizButtonModule, RouterModule.forChild(routes)],
   declarations: [SummaryComponent],
   exports: [SummaryComponent],
 })
